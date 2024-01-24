267f551aece5992708e2aca7175796494f112fe1
isaquexavier1155 <isaque.ixs@gmail.com>
2024-01-24 11:45:05 -0300

    remocao de conexao para envio de mensagem via curl

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -82,28 +82,28 @@ class AlunoController extends Controller
 
                 ////////////////////////////////////////////////INICIO INTEGRAÇÃO WHATSAPP
                 //Preciso estar com o número do whatsapp ativo para darcerto
-                $curl = curl_init();
+                // $curl = curl_init();
 
-                curl_setopt_array($curl, array(
-                CURLOPT_URL => "https://api.positus.global/v2/sandbox/whatsapp/numbers/814e1f43-af0b-4ecb-a3ee-719c3481fabf/messages",
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_ENCODING => "",
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 0,
-                CURLOPT_FOLLOWLOCATION => true,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => "POST",
-                CURLOPT_POSTFIELDS =>"{\r\n  \"to\": \"+5551997726349\",\r\n  \"type\": \"text\",\r\n  \"text\": {\r\n      \"body\": \"Mensagem de Teste php\"\r\n  }\r\n}",
-                CURLOPT_HTTPHEADER => array(
-                    "Content-Type: application/json",
-                    "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiNWY4NzVlOTExNjliMjM4MWVhOTg5Njc3NzM1NmM1M2Q0OWVjZDM0NzFiMWQzZGYzZjNhMTQzODZmODg3M2RiZDUwYzIxYzQ0MDc5ZGZmYTkiLCJpYXQiOjE3MDU0OTI1NjQuMjk2MDM2LCJuYmYiOjE3MDU0OTI1NjQuMjk2MDM5LCJleHAiOjE3MzcxMTQ5NjQuMjk0MzUxLCJzdWIiOiIzMjIwMiIsInNjb3BlcyI6W119.EBPgD3PgRMawZ6y1SSrEQQyqlDJlErG70BPrKY8Vnu4NEv0EOrcIsRDIoLMYliqTzytQQqM5_qO5aavQzwe_gumF3z-MtXzQibSbfYoO3Aw4wsYROWZb3KvjGZ8pIxEaPWakMYn8vT0UbPaIzO220DyKFTSGh0tIV68NDoHPiDn2f9F58AGOz5q8UtMaB9_9IKy4gp4AbvtYW9YXNKU10_S8JNqnu5lMrS2dCQp28QWnR2Pb1MlyhO19DkaONU9fJqpvfNmjL6ixhIwTkSXh88YYxGU7Ceuh7LiIFRp9OVGevK_yJ3xmjw29eNGKFX4rZmg-vLIV582CeJHjON_r94G5KfXf88P5gUizyaDFkvzjj_PPm8ivcmXXdk8nQNJIEEKFuQCsyQ9eNwNiWVTO-n1JIk0kvJ6uCRTWHCmaXium3Y4D62Qnm54s0HNc5mGeA6-iG1vjivUCJ9VDqW9nMFqTYHAfqq0Lu7md6Rge3zaXJPAWa5Q5j-vygap_Y96EV5ddhOBhAqHB7zeQ3sbQw2v4OwJ8EpPEBi0E3u9zh_qlTGZxDiPVEfupufKOm_iBf8cqrLXaCG3bN8tbNB9eLGtCUHabg3xkdfLNxb4TnxS1R45AQ0g9t6xQ8HWYcHdM6M9_atmsGx6RUj8Q5ZZJWIik1H-Hjq9gv5qcQFs8460"
-                ),
-                ));
+                // curl_setopt_array($curl, array(
+                // CURLOPT_URL => "https://api.positus.global/v2/sandbox/whatsapp/numbers/814e1f43-af0b-4ecb-a3ee-719c3481fabf/messages",
+                // CURLOPT_RETURNTRANSFER => true,
+                // CURLOPT_ENCODING => "",
+                // CURLOPT_MAXREDIRS => 10,
+                // CURLOPT_TIMEOUT => 0,
+                // CURLOPT_FOLLOWLOCATION => true,
+                // CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                // CURLOPT_CUSTOMREQUEST => "POST",
+                // CURLOPT_POSTFIELDS =>"{\r\n  \"to\": \"+5551997726349\",\r\n  \"type\": \"text\",\r\n  \"text\": {\r\n      \"body\": \"Mensagem de Teste php\"\r\n  }\r\n}",
+                // CURLOPT_HTTPHEADER => array(
+                //     "Content-Type: application/json",
+                //     "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiNWY4NzVlOTExNjliMjM4MWVhOTg5Njc3NzM1NmM1M2Q0OWVjZDM0NzFiMWQzZGYzZjNhMTQzODZmODg3M2RiZDUwYzIxYzQ0MDc5ZGZmYTkiLCJpYXQiOjE3MDU0OTI1NjQuMjk2MDM2LCJuYmYiOjE3MDU0OTI1NjQuMjk2MDM5LCJleHAiOjE3MzcxMTQ5NjQuMjk0MzUxLCJzdWIiOiIzMjIwMiIsInNjb3BlcyI6W119.EBPgD3PgRMawZ6y1SSrEQQyqlDJlErG70BPrKY8Vnu4NEv0EOrcIsRDIoLMYliqTzytQQqM5_qO5aavQzwe_gumF3z-MtXzQibSbfYoO3Aw4wsYROWZb3KvjGZ8pIxEaPWakMYn8vT0UbPaIzO220DyKFTSGh0tIV68NDoHPiDn2f9F58AGOz5q8UtMaB9_9IKy4gp4AbvtYW9YXNKU10_S8JNqnu5lMrS2dCQp28QWnR2Pb1MlyhO19DkaONU9fJqpvfNmjL6ixhIwTkSXh88YYxGU7Ceuh7LiIFRp9OVGevK_yJ3xmjw29eNGKFX4rZmg-vLIV582CeJHjON_r94G5KfXf88P5gUizyaDFkvzjj_PPm8ivcmXXdk8nQNJIEEKFuQCsyQ9eNwNiWVTO-n1JIk0kvJ6uCRTWHCmaXium3Y4D62Qnm54s0HNc5mGeA6-iG1vjivUCJ9VDqW9nMFqTYHAfqq0Lu7md6Rge3zaXJPAWa5Q5j-vygap_Y96EV5ddhOBhAqHB7zeQ3sbQw2v4OwJ8EpPEBi0E3u9zh_qlTGZxDiPVEfupufKOm_iBf8cqrLXaCG3bN8tbNB9eLGtCUHabg3xkdfLNxb4TnxS1R45AQ0g9t6xQ8HWYcHdM6M9_atmsGx6RUj8Q5ZZJWIik1H-Hjq9gv5qcQFs8460"
+                // ),
+                // ));
         
-                $response = curl_exec($curl);
+                // $response = curl_exec($curl);
         
-                curl_close($curl);
-                echo $response;
+                // curl_close($curl);
+                // echo $response;
 
                 //dd($response);
                 ////////////////////////////////////////////////FIM INTEGRAÇÃO WHATSAPP
