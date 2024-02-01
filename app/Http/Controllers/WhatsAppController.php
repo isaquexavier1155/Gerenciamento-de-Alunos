278e4803d4c197bb712c052e38d735474cd3be3e
isaquexavier1155 <isaque.ixs@gmail.com>
2024-02-01 15:19:46 -0300

    Ajuste parametros da mensagem enviada via API WhatsApp

--- app/Http/Controllers/WhatsAppController.php
+++ app/Http/Controllers/WhatsAppController.php
@@ -16,43 +16,53 @@ class WhatsAppController extends Controller
         // Lista de alunos com seus números de telefone
         $alunos = [
             ["nome" => "Isaque", "phone" => "+5551997726349", "pixKey" => "00020101021226830014br.gov.bcb.pix2561qrcodespix.sejaefi.com.br/v2/1b9075c3f59443ddab18f92734eb369852040000530398654041.005802BR5919Isaque6009SAO PAULO62080504txid63043C53"],
-            ["nome" => "Ana Caroline", "phone" => "+5551980388229", "pixKey" => "00020101021226830014br.gov.bcb.pix2561qrcodespix.sejaefi.com.br/v2/1b9075c3f59443ddab18f92734eb369852040000530398654041.005802BR5919Ana Caroline6009SAO PAULO62080504txid63043C53"],
+            ["nome" => "Ana Caroline", "phone" => "+5551980388229", "pixKey" => "00020101021226830014br.gov.bcb.pix2561qrcodespix.sejaefi.com.br/v2/1b9075c3f59443ddab18f92734eb369852040000530398654041.005802BR5919AnaCaroline6009SAO PAULO62080504txid63043C53"],
             // Adicione mais alunos conforme necessário
         ];
 
         $client = new Client();
 
         foreach ($alunos as $aluno) {
-            $messageTemplate = [
-                "clientApi" => $clientApi,
-                "clientSecret" => $clientSecret,
-                "phone" => $aluno["phone"],
-                "message" => "$aluno[nome],
-                    Queremos lembrá-lo sobre o pagamento da mensalidade da escola de música, que está atualmente em atraso.
-                    É de extrema importância manter os pagamentos em dia para garantir benefícios exclusivos.
-                    Pedimos que, por favor, regularize seu pagamento o mais breve possível.
-                    Se precisar de qualquer assistência ou quiser discutir opções de pagamento, entre em contato conosco.
-                    Agradecemos pela sua atenção e colaboração.
-                    
-                    PIX copia e cola:
+            // Mensagem principal sem a chave PIX
+            $mainMessage = "$aluno[nome],
+                Queremos lembrá-lo sobre o pagamento da mensalidade da escola de música, que está atualmente em atraso.
+                É de extrema importância manter os pagamentos em dia para garantir benefícios exclusivos.
+                Pedimos que, por favor, regularize seu pagamento o mais breve possível.
+                Agradecemos pela sua atenção e colaboração.";
 
-                    $aluno[pixKey]
-                    %SendFile%%Url%https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Codigo_QR.svg/1024px-Codigo_QR.svg.png%/Url%%/SendFile%",
-            ];
+            // Enviar mensagem principal
+            $this->enviarMensagem($apiUrl, $client, $clientApi, $clientSecret, $aluno["phone"], $mainMessage);
 
-            try {
-                $response = $client->post($apiUrl, [
-                    'json' => $messageTemplate,
-                ]);
+            // Mensagem separada com a chave PIX
+            $pixMessage = "PIX copia e cola: [$aluno[pixKey]]";
 
-                if ($response->getStatusCode() == 200) {
-                    echo "Mensagem enviada para {$aluno['nome']} - {$aluno['phone']}\n";
-                } else {
-                    echo "Falha ao enviar mensagem para {$aluno['nome']} - {$aluno['phone']}. Código de status: " . $response->getStatusCode() . "\n";
-                }
-            } catch (\Exception $e) {
-                echo "Erro ao enviar mensagem para {$aluno['nome']} - {$aluno['phone']}. Mensagem de erro: " . $e->getMessage() . "\n";
+            // Enviar mensagem PIX
+            $this->enviarMensagem($apiUrl, $client, $clientApi, $clientSecret, $aluno["phone"], $pixMessage);
+        }
+    }
+
+    private function enviarMensagem($apiUrl, $client, $clientApi, $clientSecret, $phone, $message)
+    {
+        $messageTemplate = [
+            "clientApi" => $clientApi,
+            "clientSecret" => $clientSecret,
+            "phone" => $phone,
+            "message" => $message,
+        ];
+
+        try {
+            $response = $client->post($apiUrl, [
+                'json' => $messageTemplate,
+            ]);
+
+            if ($response->getStatusCode() == 200) {
+                echo "Mensagem enviada para $phone\n";
+            } else {
+                echo "Falha ao enviar mensagem para $phone. Código de status: " . $response->getStatusCode() . "\n";
             }
+        } catch (\Exception $e) {
+            echo "Erro ao enviar mensagem para $phone. Mensagem de erro: " . $e->getMessage() . "\n";
         }
     }
 }
+
