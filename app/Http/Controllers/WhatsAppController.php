01a5771178dd90c47140b042dcddb770e1ab2f87
isaquexavier1155 <isaque.ixs@gmail.com>
2024-02-01 14:49:39 -0300

    Ajuste parametros da mensagem enviada via API WhatsApp

--- app/Http/Controllers/WhatsAppController.php
+++ app/Http/Controllers/WhatsAppController.php
@@ -13,31 +13,30 @@ class WhatsAppController extends Controller
         $clientApi = "api-1du2bcc52ktmgtp4546d";
         $clientSecret = "744685942";
 
-        // Lista de números de telefone
-        $numbers = ["+5551997726349", "+5551980388229"];
-
-        // Mensagem com placeholders para o número de telefone
-        $messageTemplate = [
-            "clientApi" => $clientApi,
-            "clientSecret" => $clientSecret,
-            "phone" => $numbers,
-            "message" => "	Prezado  Aluno,
-            Queremos lembrá-lo sobre o pagamento da mensalidade da escola de música, que está atualmente em atraso.
-            É de extrema importância manter os pagamentos em dia para garantir beneficios exclusivos.
-            Pedimos que, por favor, regularize seu pagamento o mais breve possível.
-            Se precisar de qualquer assistência ou quiser discutir opções de pagamento, entre em contato conosco.
-            Agradecemos pela sua atenção e colaboração.
-            PIX copia e cola: 
-        
-        00020101021226830014br.gov.bcb.pix2561qrcodespix.sejaefi.com.br/v2/1b9075c3f59443ddab18f92734eb369852040000530398654041.005802BR5919João Paulo da Silva6009SAO PAULO62080504txid63043C53
-        %SendFile%%Url%https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Codigo_QR.svg/1024px-Codigo_QR.svg.png%/Url%%/SendFile%",
+        // Lista de alunos com seus números de telefone
+        $alunos = [
+            ["nome" => "Manuela", "phone" => "+5551997726349"],
+            ["nome" => "João", "phone" => "+5551980388229"],
+            // Adicione mais alunos conforme necessário
         ];
 
         $client = new Client();
 
-        // Enviar para cada número
-        foreach ($numbers as $number) {
-            $messageTemplate["phone"] = $number;
+        foreach ($alunos as $aluno) {
+            $messageTemplate = [
+                "clientApi" => $clientApi,
+                "clientSecret" => $clientSecret,
+                "phone" => $aluno["phone"],
+                "message" => "$aluno[nome],
+                    Queremos lembrá-lo sobre o pagamento da mensalidade da escola de música, que está atualmente em atraso.
+                    É de extrema importância manter os pagamentos em dia para garantir benefícios exclusivos.
+                    Pedimos que, por favor, regularize seu pagamento o mais breve possível.
+                    Se precisar de qualquer assistência ou quiser discutir opções de pagamento, entre em contato conosco.
+                    Agradecemos pela sua atenção e colaboração.
+                    PIX copia e cola:
+                    00020101021226830014br.gov.bcb.pix2561qrcodespix.sejaefi.com.br/v2/1b9075c3f59443ddab18f92734eb369852040000530398654041.005802BR5919João Paulo da Silva6009SAO PAULO62080504txid63043C53
+                    %SendFile%%Url%https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Codigo_QR.svg/1024px-Codigo_QR.svg.png%/Url%%/SendFile%",
+            ];
 
             try {
                 $response = $client->post($apiUrl, [
@@ -45,12 +44,12 @@ class WhatsAppController extends Controller
                 ]);
 
                 if ($response->getStatusCode() == 200) {
-                    echo "Mensagem enviada para $number\n";
+                    echo "Mensagem enviada para {$aluno['nome']} - {$aluno['phone']}\n";
                 } else {
-                    echo "Falha ao enviar mensagem para $number. Código de status: " . $response->getStatusCode() . "\n";
+                    echo "Falha ao enviar mensagem para {$aluno['nome']} - {$aluno['phone']}. Código de status: " . $response->getStatusCode() . "\n";
                 }
             } catch (\Exception $e) {
-                echo "Erro ao enviar mensagem para $number. Mensagem de erro: " . $e->getMessage() . "\n";
+                echo "Erro ao enviar mensagem para {$aluno['nome']} - {$aluno['phone']}. Mensagem de erro: " . $e->getMessage() . "\n";
             }
         }
     }
